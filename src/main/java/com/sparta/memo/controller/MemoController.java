67485ebc02bde6a7551dd0cd6abb314bfcf70f21
maengjh <juhee.bda@gmail.com>
2024-10-01 18:01:52 +0900

    IoC & DI (의존성 주입을 통해 제어를 역전) - 아직 완전하지 않은 상태

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -16,8 +16,12 @@ import java.util.List;
 public class MemoController {
     private final MemoService memoService;
 
-    public MemoController(JdbcTemplate jdbcTemplate) {
-        this.memoService = new MemoService(jdbcTemplate);
+//    public MemoController(JdbcTemplate jdbcTemplate) {
+//        this.memoService = new MemoService(jdbcTemplate);
+//    }
+
+    public MemoController(MemoService memoService) {
+        this.memoService = memoService;
     }
 
     @PostMapping("/memos")
--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -11,8 +11,12 @@ import java.util.List;
 public class MemoService {
     private final MemoRepository memoRepository;
 
-    public MemoService(JdbcTemplate jdbcTemplate) {
-        memoRepository = new MemoRepository(jdbcTemplate);
+//    public MemoService(JdbcTemplate jdbcTemplate) {
+//        memoRepository = new MemoRepository(jdbcTemplate);
+//    }
+
+    public MemoService(MemoRepository memoRepository) {
+        this.memoRepository = memoRepository;
     }
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
